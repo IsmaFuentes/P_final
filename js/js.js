99ad8d863015f1e306a354617651021224385426
Ismael Fuentes <32370252+IsmaFuentes@users.noreply.github.com>
2019-05-11 21:13:27 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,18 +1,60 @@
-
-//Window onload
+// WINDOW ONLOAD
 $(document).ready(function(){
 
 	$("#fav").change(function(){
 		if(this.checked){
-			$("#fav-pelicula").fadeIn();
-			closeAlertBox();
+			$("#fav-alert").fadeIn();
+			closeAlert();
 		}
 	});
 
+	// filtros web
+	$("#apply-filters").click(function(){
+		var bar = document.getElementById("progress-bar");
+		$("#loading-screen").fadeIn();
+		barAnimate();
+
+		window.setTimeout(function(){
+			$("#loading-screen").fadeOut(300);
+			bar.style.width = "1%";
+		},1500);
+	});
+
+	//filtros modal
+	$("#apply-filters-modal").click(function(){
+		var bar = document.getElementById("progress-bar");
+		$("#loading-screen").fadeIn();
+		barAnimate();
+
+		window.setTimeout(function(){
+			$("#loading-screen").fadeOut(300);
+			bar.style.width = "1%";
+		},1500);
+	});	
+
 });
 
-function closeAlertBox(){
+// FUNCTIONS //
+
+// hace desaparecer el alert
+function closeAlert(){
 	window.setTimeout(function(){
-		$("#fav-pelicula").fadeOut(300)
+		$("#fav-alert").fadeOut(300)
 	},1000);
+}
+
+// barra de progreso
+function barAnimate(){
+	var bar = document.getElementById("progress-bar");
+	var width = 1;
+	var barfilled = false;
+
+	while(!barfilled){
+		width ++;
+		bar.style.width = width + '%';
+
+		if(width >= 100){
+			barfilled = true;
+		}
+	}
 }
