d7e9c21f9aef2b1c15d307719bd67d0536063672
Ismael Fuentes <32370252+IsmaFuentes@users.noreply.github.com>
2019-05-12 18:31:47 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
 		},1500);
 	});
 
-	//filtros modal
+	// filtros modal
 	$("#apply-filters-modal").click(function(){
 		var bar = document.getElementById("progress-bar");
 		$("#loading-screen").fadeIn();
@@ -32,6 +32,28 @@ $(document).ready(function(){
 		},1500);
 	});	
 
+	// editar perfil
+	$("#user-edit").click(function(){
+		$("#user-edit").hide();
+		$("#changes-save").show();
+		// habilita el formulario
+		enableForm();
+	});
+
+	// guardar cambios - perfil
+	$("#changes-save").click(function(){
+		// deshabilita el formulario + alert
+		if(disableForm()){
+			$("#alert-changes-success").fadeIn();
+			window.setTimeout(function(){$("#alert-changes-success").fadeOut();},1500);
+			// vuelve a mostrar el botón de editar
+			$("#changes-save").hide();
+			$("#user-edit").show();
+		}else{
+			$("#alert-changes-error").fadeIn();
+			window.setTimeout(function(){$("#alert-changes-error").fadeOut();},1500);
+		}
+	});
 });
 
 // FUNCTIONS //
@@ -56,5 +78,42 @@ function barAnimate(){
 		if(width >= 100){
 			barfilled = true;
 		}
+	}
+}
+
+function enableForm(){
+	$("#nickname").prop("disabled",false);
+	$("#email").prop("disabled",false);
+	$("#aboutme").prop("disabled",false);	
+}
+
+function disableForm(){
+	//si el formulario valida deshabilita los campos y "guarda los cambios" -> no esta programado
+	if(validateProfileForm()){
+		$("#nickname").prop("disabled",true);
+		$("#email").prop("disabled",true);
+		$("#aboutme").prop("disabled",true);	
+		return true;
+	}else {
+		return false;
+	}
+}
+
+function validateProfileForm(){
+	var nickname = $("#nickname").val();
+	var email = $("#email").val();
+	
+	// comprueba el nombre
+	var nameExpr = /^[a-z]+$/i;
+	// comprueba el email
+	var mailExpr = /^[\w\.-]+@[\w\.-]+\.\w{2,4}$/i;
+
+	var validateEmail = mailExpr.test(email);
+	var validateNickname = nameExpr.test(nickname);
+
+	if(validateEmail == true && validateNickname == true){
+		return true;
+	}else{
+		return false;
 	}
 }
